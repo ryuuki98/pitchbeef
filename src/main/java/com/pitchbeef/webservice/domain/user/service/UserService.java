664aec735e03f679fbd265755a130eb3005aac8e
ryuuki98 <jug4@naver.com>
2024-08-05 23:42:06 +0900

    add: saveUser 메소드 생성

--- src/main/java/com/pitchbeef/webservice/domain/user/service/UserService.java
+++ src/main/java/com/pitchbeef/webservice/domain/user/service/UserService.java
@@ -3,6 +3,8 @@ package com.pitchbeef.webservice.domain.user.service;
 
 import com.pitchbeef.webservice.domain.user.model.User;
 import com.pitchbeef.webservice.domain.user.repository.UserRepository;
+import com.pitchbeef.webservice.web.user.dto.UserJoinDto;
+import com.pitchbeef.webservice.web.user.dto.UserUpdateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +14,51 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+
+
+
+    //CREATE
+
+    /**
+     * 유저 저장
+     *
+     * @param userJoinDto
+     * @return user
+     */
+    public User saveUser(UserJoinDto userJoinDto) {
+        User user = createUserFromUserJoinDto(userJoinDto);
+        return userRepository.save(user);
+    }
+
+    //READ
+
+    /**
+     * 아이디로 유저 객체 검색
+     * @param userName
+     * @return User
+     */
+
     public User findByUsername(String userName){
         return userRepository.findByUsername(userName);
+    }
+
+    //UPDATE
+
+
+    //미완성
+    public User updateUser(UserUpdateDto userUpdateDto){
+        User user = userRepository.findById(userUpdateDto.getId()).orElse(null);
+        if(user != null){
+
+        }
+        return userRepository.save(user);
+    }
+
+    //DELETE
+
+
+    private static User createUserFromUserJoinDto(UserJoinDto userJoinDto) {
+        User user = new User(userJoinDto.getUsername(), userJoinDto.getEmail(), userJoinDto.getPassword(),true);
+        return user;
     }
 }
