124860cace82ab20a43679e07b3c18e9ce884286
ryuuki98 <jug4@naver.com>
2024-08-05 15:29:17 +0900

    create : 프로젝트 생성

---
+++ src/main/java/com/pitchbeef/webservice/PitchbeefApplication.java
@@ -0,0 +1,13 @@
+package com.pitchbeef.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitchbeefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PitchbeefApplication.class, args);
+    }
+
+}
