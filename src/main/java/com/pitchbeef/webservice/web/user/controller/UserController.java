7cd9b8cb7ffed8109f7cef997a9b8e7e7d23bffb
ryuuki98 <jug4@naver.com>
2024-08-13 00:05:55 +0900

    add: UserController에 join페이지 이동 , joinProcess메소드 추가

--- src/main/java/com/pitchbeef/webservice/web/user/controller/UserController.java
+++ src/main/java/com/pitchbeef/webservice/web/user/controller/UserController.java
@@ -1,9 +1,34 @@
 package com.pitchbeef.webservice.web.user.controller;
 
+import com.pitchbeef.webservice.domain.user.service.UserService;
+import com.pitchbeef.webservice.web.user.dto.UserJoinDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/user")
+@Slf4j
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/join")
+    public String join(Model model) {
+        model.addAttribute("userJoinDto", new UserJoinDto());
+        return "user/join";
+    }
+
+    @PostMapping("/joinProcess")
+    public String joinProcess(@ModelAttribute UserJoinDto userJoinDto) {
+        userService.saveUser(userJoinDto);
+        log.info("userJoinDto: {}", userJoinDto);
+        return "redirect:/user/join";
+    }
 }
