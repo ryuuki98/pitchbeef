072d5750c3b8a304d221488d7f3510e70556027d
ryuuki98 <jug4@naver.com>
2024-08-14 12:16:19 +0900

    create: UserLoginDto 생성

---
+++ src/main/java/com/pitchbeef/webservice/web/user/dto/UserLoginDto.java
@@ -0,0 +1,13 @@
+package com.pitchbeef.webservice.web.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginDto {
+    private String username;
+    private String password;
+}
