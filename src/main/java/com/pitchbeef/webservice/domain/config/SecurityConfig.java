268b30149a12e89c4c701d58caeb96b5b547281e
ryuuki98 <jug4@naver.com>
2024-08-15 18:30:21 +0900

    modify: 요청마다 jwt토큰의 유효성을 검증하는 필터 적용 , 인증 필요없는 리소스 추가

--- src/main/java/com/pitchbeef/webservice/domain/config/SecurityConfig.java
+++ src/main/java/com/pitchbeef/webservice/domain/config/SecurityConfig.java
@@ -1,31 +1,43 @@
 package com.pitchbeef.webservice.domain.config;
+
+import com.pitchbeef.webservice.domain.user.util.JwtAuthenticationFilter;
+import com.pitchbeef.webservice.domain.user.util.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtTokenProvider jwtTokenProvider) throws Exception {
         http
+                .cors(Customizer.withDefaults())  // CORS 지원을 활성화
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/index", "/user/join", "/user/joinProcess","/user/loginProcess").permitAll()
+                        .requestMatchers("/", "/index", "/user/join", "/user/login", "/js/**", "/css/**","/api/user/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form
-                        .loginPage("/user/login")
-                        .permitAll()
-                )
                 .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/") // 로그아웃 성공 시 루트로 리디렉션
                         .permitAll()
                 )
-                .csrf(csrf -> csrf.disable());
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable()); // CSRF 보호를 비활성화 (테스트 시 사용)
 
         return http.build();
     }
@@ -34,4 +46,10 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+
+
+
+
 }
